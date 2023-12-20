41956d84a498473f86004c353b0e59bfbbd9b00f
Oleg Nemanov <lego12239@yandex.ru>
2023-12-20 13:57:51 +0300

    rename hook.sh to ex-hook.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 export SV_PRG_LOGFILE_MAXSIZE=1
 export SV_LOGPATH=tmp
 export SV_PIDPATH=tmp
-export SV_HOOK=./hook.sh
+export SV_HOOK=./ex-hook.sh
 
 rm tmp/*
 echo "run prg1 one ./prg.sh" | ./sv.sh env1
